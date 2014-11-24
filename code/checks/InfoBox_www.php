14794aa986bc435901ab56d1cd718164207d4714
Stan Hutcheon <stanhutcheon@bigfork.co.uk>
2014-11-24 12:28:58 +0000

    Make sure the www check works

--- code/checks/InfoBox_www.php
+++ code/checks/InfoBox_www.php
@@ -5,7 +5,7 @@ class InfoBox_www implements InfoBox {
 	public function show() {
 		$url = Director::absoluteBaseURL();
 
-		if(preg_match("/^https?:\/\/www./", $url) == 1) {
+		if(preg_match("/^https?:\/\/www./", $url) == 0) {
 			return true;
 		}
 
